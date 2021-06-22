15cb393ba60b1374d294cd774fe34eec3824afd4
TatchumNono <tatchumnono@gmail.com>
2021-06-22 23:30:49 +0200

    Created the cart styled component

--- src/App.js
+++ src/App.js
@@ -1,97 +1,145 @@
-import './App.css';
+import "./App.css";
 import {
   Search,
   Button,
   CategoryButton,
   Card,
-} from './components/StyledComponents';
-import Pic from './pic.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faHistory } from '@fortawesome/free-solid-svg-icons';
+  CardImg,
+  CartCard,
+} from "./components/StyledComponents";
+import Pic from "./pic.jpg";
+import Wine7 from "./wine7.png";
+import Wine8 from "./wine8.png";
+import Wine5 from "./wine5.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faHistory,
+  faPlus,
+  faMinus,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   //const categories = ['Red', 'white', 'Rose', 'Sparkling', 'Dessert'];
   const products = [
     {
-      name: 'Casa Castillo Monastrell',
-      price: '$20',
-      country: 'Spain',
-      bottleSize: '750ml',
+      name: "Casa Castillo Monastrell",
+      price: "$20",
+      country: "Spain",
+      bottleSize: "750ml",
+      pic: Wine5,
     },
     {
-      name: 'Littlebury Road',
-      price: '$16',
-      country: 'USA',
-      bottleSize: '750ml',
+      name: "Littlebury Road",
+      price: "$16",
+      country: "USA",
+      bottleSize: "750ml",
+      pic: Wine5,
     },
     {
-      name: 'Neleman Monastrell',
-      price: '$16',
-      country: 'Italy',
-      bottleSize: '750ml',
+      name: "Neleman Monastrell",
+      price: "$16",
+      country: "Italy",
+      bottleSize: "750ml",
+      pic: Wine5,
     },
     {
-      name: 'Scantianum Toscana',
-      price: '$32',
-      country: 'Italy',
-      bottleSize: '750ml',
+      name: "Scantianum Toscana",
+      price: "$32",
+      country: "Italy",
+      bottleSize: "750ml",
+      pic: Wine5,
     },
     {
-      name: 'Marques de Tomares',
-      price: '$18',
-      country: 'Spain',
-      bottleSize: '750ml',
+      name: "Marques de Tomares",
+      price: "$18",
+      country: "Spain",
+      bottleSize: "750ml",
+      pic: Wine5,
     },
     {
-      name: 'Beaujolais de lantigne',
-      price: '$24',
-      country: 'France',
-      bottleSize: '750ml',
+      name: "Beaujolais de lantigne",
+      price: "$24",
+      country: "France",
+      bottleSize: "750ml",
+      pic: Wine5,
+    },
+  ];
+
+  const cart = [
+    {
+      name: "Casa Castillo Monastrell",
+      price: "$20",
+      country: "Spain",
+      bottleSize: "750ml",
+      pic: Wine5,
+    },
+    {
+      name: "Littlebury Road",
+      price: "$16",
+      country: "USA",
+      bottleSize: "750ml",
+      pic: Wine7,
+    },
+    {
+      name: "Neleman Monastrell",
+      price: "$16",
+      country: "Italy",
+      bottleSize: "750ml",
+      pic: Wine8,
     },
   ];
 
   return (
-    <div className="App">
-      <div className="container">
-        <div className="Left">
+    <div className='App'>
+      <div className='container'>
+        <div className='Left'>
           <nav>
-            <div className="title">
+            <div className='title'>
               <h1>Wine & Spirits Team</h1>
-              <p className="small">Location: Yaounde</p>
+              <div className='location'>
+                <p className='small'>Location: Yaounde</p>
+              </div>
             </div>
-            <div className="icons">
+            <div className='icons'>
               <FontAwesomeIcon
                 icon={faHistory}
-                size="3x"
+                size='3x'
                 style={{
-                  backgroundColor: 'rgb(174, 245, 245)',
-                  padding: '0.5rem',
-                  borderRadius: '12px',
-                  fontSize: '1em',
+                  margin: "0.5rem",
+                  backgroundColor: "rgb(174, 245, 245)",
+                  padding: "0.5rem",
+                  borderRadius: "12px",
+                  fontSize: "0.9rem",
                 }}
               />
               <FontAwesomeIcon
                 icon={faBell}
-                size="3x"
-                outlined
+                size='3x'
                 style={{
-                  backgroundColor: 'rgb(174, 245, 245)',
-                  padding: '0.5rem',
-                  borderRadius: '12px',
-                  fontSize: '1em',
+                  margin: "0.5rem",
+                  backgroundColor: "rgb(174, 245, 245)",
+                  padding: "0.5rem",
+                  borderRadius: "12px",
+                  fontSize: "0.9rem",
                 }}
               />
             </div>
-            <div className="user">
-              <img src={Pic} alt="profile" />
-              <p>Cody Fisher</p>
+            <div className='user'>
+              <img src={Pic} alt='profile' />
+              <div className='info'>
+                <p>Cody Fisher</p>
+                <div className='position'>
+                  <p className='small'>Employee</p>
+                </div>
+              </div>
             </div>
           </nav>
-          <div className="search">
-            <Search placeholder="Find products by name or code" />
+          <div className='search'>
+            <Search placeholder='Find products by name or code' />
           </div>
 
-          <div className="categories">
+          <div className='categories'>
             <CategoryButton primary>Red</CategoryButton>
             <CategoryButton>White</CategoryButton>
             <CategoryButton>Rose</CategoryButton>
@@ -99,30 +147,72 @@ function App() {
             <CategoryButton>Dessert</CategoryButton>
           </div>
 
-          <div className="products">
+          <div className='products'>
             {products.map((product) => (
               <Card>
-                <div className="innerCard">
-                  <div className="cardImg"></div>
-                  <div className="cardDetails">
-                    {' '}
-                    <h4>{product.name}</h4>
-                    <p className="small">
-                      Country:{product.country} Bottle size:{product.bottleSize}
+                <div className='innerCard'>
+                  <CardImg img={product.pic}></CardImg>
+                  <div className='cardDetails'>
+                    {" "}
+                    <h4 className='name'>{product.name}</h4>
+                    <p className='small'>
+                      {`Country : ${product.country} • Bottle size : ${product.bottleSize}`}
                     </p>
-                    <h4 className="price">{product.price}</h4>
+                    <h4 className='price'>{product.price}</h4>
                   </div>
                 </div>
               </Card>
             ))}
           </div>
         </div>
-        <div className="Right">
-          <div className="head">
+        <div className='Right'>
+          <div className='head'>
             <h1>Cart</h1>
-            <div className="sub">
+            <div className='sub'>
               <Button>Clear</Button>
             </div>
+          </div>
+          <div className='cart'>
+            {cart.map((item) => (
+              <>
+                <CartCard img={item.pic}></CartCard>
+                <div className='cardDetails'>
+                  <h4 className='name'>{item.name}</h4>
+                  <p className='small'>{`Bottle size : ${item.bottleSize}`}</p>
+                  <h4 className='price'>{item.price}</h4>
+                </div>
+                <div className='cardControls'>
+                  <FontAwesomeIcon
+                    icon={faPlus}
+                    size='1x'
+                    fill
+                    style={{
+                      backgroundColor: "rgb(174, 245, 245)",
+                      padding: "0.4rem",
+                      borderRadius: "10px",
+                      fontSize: "0.6em",
+                    }}
+                  />
+
+                  <h4>2</h4>
+
+                  <FontAwesomeIcon
+                    icon={faMinus}
+                    size='1x'
+                    fill
+                    style={{
+                      backgroundColor: "rgb(174, 245, 245)",
+                      padding: "0.4rem",
+                      borderRadius: "10px",
+                      fontSize: "0.6em",
+                    }}
+                  />
+                </div>
+              </>
+            ))}
+          </div>
+          <div>
+            <button>Charge</button>
           </div>
         </div>
       </div>
@@ -131,15 +221,3 @@ function App() {
 }
 
 export default App;
-
-/* <FontAwesomeIcon
-icon={faSearch}
-size="3x"
-outlined
-style={{
-  backgroundColor: 'rgb(174, 245, 245)',
-  padding: '0.5rem',
-  borderRadius: '12px',
-  fontSize: '1em',
-}}
-/> */
--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from "styled-components";
 
 export const Search = styled.input`
   width: 95%;
@@ -25,8 +25,8 @@ export const CategoryButton = styled.button`
   margin-right: 15px;
   border-radius: 20px;
   border: none;
-  color: ${(props) => (props.primary ? '#f9f9f9' : '#ee6e7a')};
-  background-color: ${(props) => (props.primary ? '#ee6e7a' : '#f9f9f9')};
+  color: ${(props) => (props.primary ? "#f9f9f9" : "#ee6e7a")};
+  background-color: ${(props) => (props.primary ? "#ee6e7a" : "#f9f9f9")};
   box-shadow: -4px 6px 41px -14px rgba(0, 0, 0, 0.9);
   &:hover {
     transition: 1s;
@@ -43,4 +43,34 @@ export const Card = styled.div`
   padding: 0.4rem;
   background-color: #f9f9f9;
   box-shadow: -4px 6px 41px -35px rgba(0, 0, 0, 0.9);
+`;
+
+export const CartCard = styled.div`
+  width: 5rem;
+  height: 2rem;
+  border-radius: 15px;
+  border: none;
+  padding: 1rem;
+  padding-bottom: 3rem;
+  margin-top: 1.5rem;
+  background-image: url(${(props) => props.img});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: rgb(253, 232, 236);
+  box-shadow: -4px 6px 41px -35px rgba(0, 0, 0, 0.9);
+`;
+
+export const CardImg = styled.div`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 20px;
+  border: none;
+  padding: 0.5rem;
+  /*background-color: #ee6e7a;*/
+  background-color: rgb(253, 232, 236);
+  margin-left: 1rem;
+  margin-top: 1.3rem;
+  background-image: url(${(props) => props.img});
+  background-repeat: no-repeat;
+  background-position: center;
 `;
